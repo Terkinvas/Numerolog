ee94e7d60817ee7dca382a5ad0a136d2128e1f67
Terkinvas <101996435+Terkinvas@users.noreply.github.com>
2022-04-13 22:58:04 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+console.log ('Hello')
+
+let day1= 0
+let day2= 4
+let month1= 0
+let month2= 9
+let year1=1
+let year2=9
+let year3=8
+let year4=6
+
+
+
+
+const c1 = day1+ day2+ month1 + month2 + year1 + year2+ year3 + year4
+const a=2
+let c2= c1-a
+
+let B1= ( c1 % 10 );
+
+
+console.log(c1)
+console.log(c2)
+console.log(B1)
