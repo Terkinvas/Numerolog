b9aa12d308fa0054f7774db345ba02540b5d8f12
Terkinvas <101996435+Terkinvas@users.noreply.github.com>
2022-04-14 22:57:54 +0300

    Update app.js

--- app.js
+++ app.js
@@ -9,16 +9,33 @@ let year2=9
 let year3=8
 let year4=6
 
+let c1 = day1+ day2+ month1 + month2 + year1 + year2+ year3 + year4
+
+const a=2;
+let c2=c1-a;
+console.log (c1)
+console.log (c2)
+
+let b1= (c1%10); 
+let b3 = (c1-b1);
+console.log (b3)
+let b6 = (b3/10)
+console.log (b6)
+console.log (b1)
+
+let b4= b1+b6
+console.log (b4)
+
+let d1= (c2%10); 
+let d3 = (c2-d1);
+console.log (d3)
+let d6 = (d3/10)
+console.log (d6)
+console.log (d1)
+
+let d4= d1+d6 ;
+console.log (d4)
 
 
 
-const c1 = day1+ day2+ month1 + month2 + year1 + year2+ year3 + year4
-const a=2
-let c2= c1-a
 
-let B1= ( c1 % 10 );
-
-
-console.log(c1)
-console.log(c2)
-console.log(B1)
