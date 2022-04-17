0a7d16daeadaaef852984367c4faad2599d28ca1
Terkinvas <101996435+Terkinvas@users.noreply.github.com>
2022-04-17 20:13:45 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
-console.log ('Hello')
+console.log ('Hello');
+
+const button1=document.querySelector("#button1");
+const button2=document.querySelector("#button2");
+const button3=document.querySelector("#button3");
+const button4=document.querySelector("#button4");
+const button5=document.querySelector("#button5");
+const button6=document.querySelector("#button6");
+const button7=document.querySelector("#button7");
+const button8=document.querySelector("#button8");
+const button9=document.querySelector("#button9");
+const button0=document.querySelector("#button0");
+const button11=document.querySelector("#button11");
+const testbtn=document.querySelector("#testbtn");
 
 let day1= 2
 let day2= 9
@@ -14,7 +27,7 @@ console.log(day1,day2,month1,month2,year1,year2,year3,year4)
 
 
 let c1 = day1+ day2+ month1 + month2 + year1 + year2+ year3 + year4
-console.log (c1)
+
 
 if (year1 < 2) {
 
@@ -48,4 +61,41 @@ let e4= e1+e6 ;
 console.log ( g6, g1, g4, e6, e1,e4)
 
 }
+button1.addEventListener("click", function() {
+console.log("1")
+})
+button2.addEventListener("click", function() {
+    console.log("2")
+    })
+
+
+button3.addEventListener("click", function() {
+    console.log("3")
+    })
+
+    button4.addEventListener("click", function() {
+        console.log("4")
+        })
+        button5.addEventListener("click", function() {
+            console.log("5")
+            })
+            button6.addEventListener("click", function() {
+                console.log("6")
+                })
+                button7.addEventListener("click", function() {
+                    console.log("7")
+                    })
+                    button8.addEventListener("click", function() {
+                        console.log("8")
+                        })
+                        button9.addEventListener("click", function() {
+                            console.log("9")
+                            })
+                            button0.addEventListener("click", function() {
+                                console.log("0")
+                                })
+                                button11.addEventListener("click", function() {
+                                    console.log("11")
+                                    })
+                                  
 
